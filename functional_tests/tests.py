81de2c733ffb98f2dd93ff0376ec30ccbaff435d
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-12 13:22:17 -0700

    Build regressions for multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,7 @@ from selenium.common.exceptions import WebDriverException
 import time
 
 
-MAX_WAIT = 55555
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -28,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Cassandra has heard about a cool new online to-do app. She goes
         # to check out its homepage:
         self.browser.get(self.live_server_url)
@@ -61,16 +61,58 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
         self.wait_for_row_in_list_table(
             "2: Use peacock feathers to make a fly"
         )
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # Cassandra wonders whether the site will remember her list. Then she
         # sees that the site has generated a unique URL for her -- there is
         # some explanatory text to that effect.
-        self.fail("Finish the test!")
-
-        # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to dreaming
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Cassandra starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+
+        # She notices that her list has a unique URL
+        cassandra_list_url = self.browser.current_url
+        self.assertRegex(cassandra_list_url, "/lists/.+")
+
+        # Now a new user, Sophia, comes along to the site.
+
+        # NOTE: We use a new browser session to make sure that no information
+        # NOTE: of Cassandra's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Sophia visits the home page. There is no sign of Cassandra's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+
+        # Sophia starts a new list by entering a new item. She
+        # is less interesting than Cassandra...
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+        # Sophia gets her own unique URL
+        sophia_list_url = self.browser.current_url
+        self.assertRegex(sophia_list_url, "/lists/.+")
+        self.assertNotEqual(sophia_list_url, cassandra_list_url)
+
+        # Again, there is no trace of Cassandra's list
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
+
+        # Satisfied, they both go back to dreaming
