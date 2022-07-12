03f2b03150cc8d1242fee2531080e42142b1f817
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-12 14:13:37 -0700

    Add new URL, view, and template to display lists

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     # path("admin/", admin.site.urls),
     # path(r"^$", views.home_page, name="home"),
     path("", views.home_page, name="home"),
+    path(
+        "lists/the-only-list-in-the-world/", views.view_list, name="view_list"
+    ),
 ]
