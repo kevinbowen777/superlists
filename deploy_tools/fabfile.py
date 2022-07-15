85ef9b8e6b9980f7564905cc77ddf7b1732b6ce8
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-15 12:52:36 -0700

    Comment out git reset. Not capturing correct commit hash

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -27,8 +27,12 @@ def _get_latest_source(source_folder):
         run(f"cd {source_folder} && git fetch")
     else:
         run(f"git clone {REPO_URL} {source_folder}")
-    current_commit = local("git log -n 1 --format=%H", capture=True)
-    run(f"cd {source_folder} && git reset --hard {current_commit}")
+    # This doesn't seem to grab the same commit hash as when the
+    # command is run from the command line?
+    current_commit = local(  # noqa:F841
+        "git log -n 1 --format=%H", capture=True
+    )
+    # run(f'cd {source_folder} && git reset --hard {current_commit}')
 
 
 def _update_settings(source_folder, site_name):
