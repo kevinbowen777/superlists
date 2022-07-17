eae5e6eef535c650ed44925bea310c1fe8e1d346
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-16 22:29:46 -0700

    Create helper function for css selector

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.keys import Keys
 
 
 class ItemValidationTest(FunctionalTest):
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector(".has-error")
+
     def test_cannot_add_empty_list_items(self):
         # Cassandra goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -64,7 +67,7 @@ class ItemValidationTest(FunctionalTest):
         # She sees a helpful error message
         self.wait_for(
             lambda: self.assertEqual(
-                self.browser.find_element_by_css_selector(".has-error").text,
+                self.get_error_element().text,
                 "You've already got this in your list",
             )
         )
@@ -79,11 +82,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(
-            lambda: self.assertTrue(
-                self.browser.find_element_by_css_selector(
-                    ".has-error"
-                ).is_displayed()
-            )
+            lambda: self.assertTrue(self.get_error_element().is_displayed())
         )
 
         # She starts typing in the input box to clear the error
@@ -91,9 +90,5 @@ class ItemValidationTest(FunctionalTest):
 
         # She is pleased to see that the error message disappears
         self.wait_for(
-            lambda: self.assertFalse(
-                self.browser.find_element_by_css_selector(
-                    ".has-error"
-                ).is_displayed()
-            )
+            lambda: self.assertFalse(self.get_error_element().is_displayed())
         )
